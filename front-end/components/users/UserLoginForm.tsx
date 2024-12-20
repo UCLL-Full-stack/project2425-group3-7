6e3eee4ca31718d4fc341c488ec2970b82854ebc
DikkieDik05 <145990039+DikkieDik05@users.noreply.github.com>
2024-12-20 20:25:41 +0100

    analysis

--- front-end/components/users/UserLoginForm.tsx
+++ front-end/components/users/UserLoginForm.tsx
@@ -136,7 +136,7 @@ const UserLoginForm: React.FC = () => {
           </div>
         </div>
         <button
-          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+          className="bg-oranje text-white px-4 py-2 rounded-lg shadow-md"
           type="submit"
         >
           {t('login.button')}
